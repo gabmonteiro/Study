4cf2232e0bd19e2c804f40a074a0cc2fd9053667
Gabriel Monteiro <gabrieldealmeidamonteiro@gmail.com>
2022-09-26 16:30:18 -0300

    Começando os estudos em javascript

---
+++ javascript/interactive.js
@@ -0,0 +1,9 @@
+// pega o conteúdo da tag h1 e aplica uma condição
+let titulo = document.getElementById("titulo").innerText;
+
+if (titulo == "Javascript") {
+  alert("Sucesso!");
+  alert(titulo);
+}
+
+confirm("Escolha uma das duas opções");
